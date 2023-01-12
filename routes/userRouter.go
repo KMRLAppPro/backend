7e490626ad94b10cfd02be1d02c28e464dd61099
KMRLAppPro <119995500+KMRLAppPro@users.noreply.github.com>
2023-01-12 19:53:43 +0530

    Update userRouter.go

--- routes/userRouter.go
+++ routes/userRouter.go
@@ -2,13 +2,13 @@ package routes
 
 import (
 	controller "github.com/KMRLAppPro/backend/controllers"
-	"github.com/KMRLAppPro/backend/middleware"
+	//"github.com/KMRLAppPro/backend/middleware"
 
 	"github.com/gin-gonic/gin"
 )
 
 func UserRoutes(incomingRoutes *gin.Engine) {
-	incomingRoutes.Use(middleware.Authenticate())
+	//incomingRoutes.Use(middleware.Authenticate())
 	incomingRoutes.POST("/upload/:image_name",controller.UploadFile())
 	incomingRoutes.GET("/download/:image_name",controller.DownloadFile())
 }
